8b3f7792f04f4cc7350539e91a2e215f2160a7be
Satoki TSUJINO <satoki@rain.hyarc.nagoya-u.ac.jp>
2020-11-13 17:18:14 +0900

    Modofication procedures in calculation Mtangent.

--- assimilation/lorenz63.jl
+++ assimilation/lorenz63.jl
@@ -1,3 +1,97 @@
+# Lorenz (1963) モデルで同化・解析処理を行うシミュレーション
+# Author: Satoki Tsujino (satoki_at_gfd-dennou.org)
+# Date: 2020/11/01
+# License: LGPL2.1
+#############################
+# functions for calculation #
+#############################
+
+function L63_tendency(x,cx,cy,cz)  # calculation of tendency in Lorenz (1963)
+    # x: 状態変数
+    # c{x,y,z}: 各時間発展方程式のパラメータ (定数) 
+    m_res = fill(0.0,3)
+    
+    # i=1
+    m_res[1] = - cx * (x[1] - x[2])
+    # i=2
+    m_res[2] = - x[2] - x[1] * x[3] + cy * x[1]
+    # i=3
+    m_res[3] = - cz * x[3] + x[1] * x[2]
+    
+    return m_res
+end
+
+function L63_EU1(x,dt,cx,cy,cz)  # Lorenz (1963) with explicit Euler scheme
+    # x: 状態変数
+    # dt: 時間ステップ
+    # c{x,y,z}: 各時間発展方程式のパラメータ (定数) 
+    m_res = fill(0.0,3)
+    
+    m_res = x + dt * L63_tendency(x,cx,cy,cz)
+    
+    return m_res
+end
+
+function L63_RK4(x,dt,cx,cy,cz)  # Lorenz (1963) with the 4th-order Runge-Kutta scheme
+    # x: 状態変数
+    # dt: 時間ステップ
+    # c{x,y,z}: 各時間発展方程式のパラメータ (定数) 
+    m_res = fill(0.0,3)
+    
+    k1 = L63_tendency(x,cx,cy,cz)
+    k2 = L63_tendency(x+0.5*dt*k1,cx,cy,cz)
+    k3 = L63_tendency(x+0.5*dt*k2,cx,cy,cz)
+    k4 = L63_tendency(x+dt*k3,cx,cy,cz)
+    
+    m_res = x + dt * (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0
+    
+    return m_res
+end
+
+function L63_tangent(x,dt,cx,cy,cz)  # The tangential operator for L63_full
+    # x: 状態変数
+    # dt: 時間ステップ
+    # c{x,y,z}: 各時間発展方程式のパラメータ (定数) 
+    mt_res = fill(0.0,3,3)
+    
+    mt_res[1,1] = 1.0 - cx * dt
+    mt_res[2,2] = 1.0 - dt
+    mt_res[3,3] = 1.0 - cz * dt
+    mt_res[1,2] = cx * dt
+    mt_res[2,1] = (cy - x[3]) * dt
+    mt_res[2,3] = - x[1] * dt
+    mt_res[3,1] = x[2] * dt
+    mt_res[3,2] = x[1] * dt
+    
+    return mt_res
+end
+
+function L63_TL_check(x,deltax,dt,nt,cx,cy,cz)  # The TL check for L63_tangent
+    # x: 状態変数
+    # deltax: x に対する微小変数
+    # dt: 時間ステップ
+    # nt: 検証する時間ステップ数
+    # c{x,y,z}: 各時間発展方程式のパラメータ (定数) 
+    xt = x
+    delx = deltax
+    println("delx ",delx)
+    
+    for i in 1:nt
+        Lxt = L63_EU1(xt,dt,cx,cy,cz)  # Time integration for x
+        Lxtd = L63_EU1(xt+delx,dt,cx,cy,cz)  # Time integration for x + deltax
+        Ldx = Lxtd - Lxt
+        Mop = L63_tangent(xt,dt,cx,cy,cz)  # TLM for x
+        Mdx = Mop * delx
+        println("TL check:i=",i,", (dL/dx)δx=",Ldx'*Ldx,", Mδx=",Mdx'*Mdx)
+        
+        # Updating x and deltax
+        xt = Lxt
+        delx = Mdx
+        println("dx check ",delx' * delx)
+    end
+    
+end
+
 # Lorenz (1963) モデルで各時刻に同化・解析処理を挟むシミュレーション
 using PyPlot
 using Random
@@ -44,7 +138,7 @@ delta_x = reshape(zeros(nx,1),nx,1)
 # Setting parameters
 xinit = [10.0, 20.0, 30.0]
 dt = 0.00001
-delx = 0.1  # small departure for the TL/AD check
+delx = 0.1  # small departure for the TL check
 k = 10.0
 r = 23.0
 b = 8.0/3.0
@@ -63,8 +157,8 @@ lam_max[1] = 1.0
 evec_max = fill(1.0,nx,nt)
 eps_inv = 1.0 / eps
 
-# TL/AD check (If you need to check the Mop, please activate the next line.)
-# L63_TLAD_check(x_t[1:nx,1],delx.*fill(1.0,nx,1),dt,100,k,r,b)
+# TL check (If you need to check the Mop, please activate the next line.)
+# L63_TL_check(x_t[1:nx,1],delx.*fill(1.0,nx,1),dt,100,k,r,b)
 
 for i in 1:nt-1
     if mod(i-1,n_ascyc) == 0  # Entering the analysis processes
