00a1ec2e734b614d4f80a0d4e41af394b1e878c4
Satoki TSUJINO <satoki@rain.hyarc.nagoya-u.ac.jp>
2020-11-20 17:42:43 +0900

    Rearrange functions.

--- assimilation/KF/lorenz63.jl
+++ assimilation/KF/lorenz63.jl
@@ -1,98 +1,15 @@
-# Lorenz (1963) モデルで同化・解析処理を行うシミュレーション
+# KF assimilation simulation in Lorenz (1963) model.
 # Author: Satoki Tsujino (satoki_at_gfd-dennou.org)
 # Date: 2020/11/01
+# Modification: 2020/11/20
 # License: LGPL2.1
 #############################
 # functions for calculation #
 #############################
 
-function L63_tendency(x,cx,cy,cz)  # calculation of tendency in Lorenz (1963)
-    # x: 状態変数
-    # c{x,y,z}: 各時間発展方程式のパラメータ (定数) 
-    m_res = fill(0.0,3)
-    
-    # i=1
-    m_res[1] = - cx * (x[1] - x[2])
-    # i=2
-    m_res[2] = - x[2] - x[1] * x[3] + cy * x[1]
-    # i=3
-    m_res[3] = - cz * x[3] + x[1] * x[2]
-    
-    return m_res
-end
+include("./KF/lorenz63_module.jl")
 
-function L63_EU1(x,dt,cx,cy,cz)  # Lorenz (1963) with explicit Euler scheme
-    # x: 状態変数
-    # dt: 時間ステップ
-    # c{x,y,z}: 各時間発展方程式のパラメータ (定数) 
-    m_res = fill(0.0,3)
-    
-    m_res = x + dt * L63_tendency(x,cx,cy,cz)
-    
-    return m_res
-end
-
-function L63_RK4(x,dt,cx,cy,cz)  # Lorenz (1963) with the 4th-order Runge-Kutta scheme
-    # x: 状態変数
-    # dt: 時間ステップ
-    # c{x,y,z}: 各時間発展方程式のパラメータ (定数) 
-    m_res = fill(0.0,3)
-    
-    k1 = L63_tendency(x,cx,cy,cz)
-    k2 = L63_tendency(x+0.5*dt*k1,cx,cy,cz)
-    k3 = L63_tendency(x+0.5*dt*k2,cx,cy,cz)
-    k4 = L63_tendency(x+dt*k3,cx,cy,cz)
-    
-    m_res = x + dt * (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0
-    
-    return m_res
-end
-
-function L63_tangent(x,dt,cx,cy,cz)  # The tangential operator for L63_full
-    # x: 状態変数
-    # dt: 時間ステップ
-    # c{x,y,z}: 各時間発展方程式のパラメータ (定数) 
-    mt_res = fill(0.0,3,3)
-    
-    mt_res[1,1] = 1.0 - cx * dt
-    mt_res[2,2] = 1.0 - dt
-    mt_res[3,3] = 1.0 - cz * dt
-    mt_res[1,2] = cx * dt
-    mt_res[2,1] = (cy - x[3]) * dt
-    mt_res[2,3] = - x[1] * dt
-    mt_res[3,1] = x[2] * dt
-    mt_res[3,2] = x[1] * dt
-    
-    return mt_res
-end
-
-function L63_TL_check(x,deltax,dt,nt,cx,cy,cz)  # The TL check for L63_tangent
-    # x: 状態変数
-    # deltax: x に対する微小変数
-    # dt: 時間ステップ
-    # nt: 検証する時間ステップ数
-    # c{x,y,z}: 各時間発展方程式のパラメータ (定数) 
-    xt = x
-    delx = deltax
-    println("delx ",delx)
-    
-    for i in 1:nt
-        Lxt = L63_EU1(xt,dt,cx,cy,cz)  # Time integration for x
-        Lxtd = L63_EU1(xt+delx,dt,cx,cy,cz)  # Time integration for x + deltax
-        Ldx = Lxtd - Lxt
-        Mop = L63_tangent(xt,dt,cx,cy,cz)  # TLM for x
-        Mdx = Mop * delx
-        println("TL check:i=",i,", (dL/dx)δx=",Ldx'*Ldx,", Mδx=",Mdx'*Mdx)
-        
-        # Updating x and deltax
-        xt = Lxt
-        delx = Mdx
-        println("dx check ",delx' * delx)
-    end
-    
-end
-
-# Lorenz (1963) モデルで各時刻に同化・解析処理を挟むシミュレーション
+using .Lorenz63_functions
 using Random
 using LinearAlgebra
 
@@ -181,7 +98,7 @@ for i in 1:nt-1
     #-- Forecast
     t[i] = dt*(i-1)
     
-    Mop_linf = L63_tangent(x_an,dt,k,r,b)
+    Mop_linf = L63_EU1_tangent(x_an,dt,k,r,b)
     #for j in 1:nx
     #    Mop_linf[1:nx,j] = eps_inv .* (L63_RK4(x_an_lin + eps .* I_mat[1:nx,j],dt,k,r,b) - L63_RK4(x_an_lin,dt,k,r,b))
     #end
@@ -213,25 +130,37 @@ t[nt] = t[nt-1] + dt
 using PyPlot
 
 fig = figure("pyplot_majorminor",figsize=(7,5))
-##p = plot(t[1:nt],x_t[1,1:nt],color="black",label="Perfect")
-##p = plot(t[1:nt],x_e[1,1:nt],color="blue",label="False")
-##p = plot(t[1:nt],x_f[1,1:nt],color="red",label="Assim")
-##p = plot(t_o[1:div(nt-1,n_ascyc)+1],y_o[1,1:div(nt-1,n_ascyc)+1],linestyle="",marker="o",color="black",label="Obs")
-#p = plot(x_t[1,1:nt],x_t[3,1:nt],color="black",label="Perfect")
-#p = plot(x_e[1,1:nt],x_e[3,1:nt],color="blue",label="False")
-#p = plot(x_f[1,1:nt],x_f[3,1:nt],color="red",label="Assim")
-#p = plot(y_o[1,1:div(nt-1,n_ascyc)+1],y_o[3,1:div(nt-1,n_ascyc)+1],linestyle="",marker="o",color="black",label="Obs")
-p = plot(t[1:nt],Egtime[1:nt]*0.01,color="black",label="Tr(Pf)")
-p = plot(t[1:nt],L2Ntime[1:nt],color="red",label="L2norm")
-p = plot(t_o[1:div(nt-1,n_ascyc)+1],fill(0.0,div(nt-1,n_ascyc)+1),linestyle="",marker="o",color="black",label="Obs")
-###p = plot(t[1:nt],lam_max[1:nt],color="green",label="Lambda_max")
+draw_num = 3
+if draw_num == 1
+    p = plot(t[1:nt],x_t[1,1:nt],color="black",label="Perfect")
+    p = plot(t[1:nt],x_e[1,1:nt],color="blue",label="False")
+    p = plot(t[1:nt],x_f[1,1:nt],color="red",label="Assim")
+    p = plot(t_o[1:div(nt-1,n_ascyc)+1],y_o[1,1:div(nt-1,n_ascyc)+1],linestyle="",marker="o",color="black",label="Obs")
+elseif draw_num == 2
+    p = plot(x_t[1,1:nt],x_t[3,1:nt],color="black",label="Perfect")
+    p = plot(x_e[1,1:nt],x_e[3,1:nt],color="blue",label="False")
+    p = plot(x_f[1,1:nt],x_f[3,1:nt],color="red",label="Assim")
+    p = plot(y_o[1,1:div(nt-1,n_ascyc)+1],y_o[3,1:div(nt-1,n_ascyc)+1],linestyle="",marker="o",color="black",label="Obs")
+elseif draw_num == 3
+    p = plot(t[1:nt],Egtime[1:nt]*0.01,color="black",label="Tr(Pf)")
+    p = plot(t[1:nt],L2Ntime[1:nt],color="red",label="RMSE(x-xt)")
+    p = plot(t_o[1:div(nt-1,n_ascyc)+1],fill(0.0,div(nt-1,n_ascyc)+1),linestyle="",marker="o",color="black",label="Obs")
+    #p = plot(t[1:nt],lam_max[1:nt],color="green",label="Lambda_max")
+end
 legend()
 ax = gca()
-xlabel("Time")
-ylabel("ΔX")
-##ylabel("X^T")
-#xlabel("X")
-#ylabel("Z")
+
+if draw_num == 1
+    xlabel("Time")
+    ylabel("X^T")
+elseif draw_num == 2
+    xlabel("Time")
+    ylabel("ΔX")
+elseif draw_num == 3
+    xlabel("X")
+    ylabel("Z")
+end
+
 grid("on")
 PyPlot.title("Lorenz63")
 
